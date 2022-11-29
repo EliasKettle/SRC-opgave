bc13e83055ceb9005c27d72e36a07650399ae89f
Elias Christiansen <eliaslieberoth@gmail.com>
2022-11-29 12:56:10 +0100

    elipser virker

--- main.py
+++ main.py
@@ -3,36 +3,88 @@ import math
 
 BREDDE = 1250
 HOEJDE = 650
-SPORLAENGDE = 250
+SPORLAENGDE = 50
 
 
 
-
-def ellipse(tid, centrum, radius, vinkelhastighed, fase=0):
+def ellipse_mercury(tid, centrum, a, b,vinkelhastighed, fase=0):
     x_c, y_c = centrum
-    x = x_c + radius * math.cos(vinkelhastighed * tid + fase+500)
-    y = y_c + radius * math.sin(vinkelhastighed * tid + fase)
+    x = x_c + a * math.cos(vinkelhastighed * tid + fase)
+    y = y_c + b * math.sin(vinkelhastighed * tid + fase)
     return x, y
 
-def tegn_baner(delta_tid):
-    arcade.start_render()
-    x, y = ellipse(tegn_baner.tid, (BREDDE / 2, HOEJDE / 2), 100, 1, 0)
-    arcade.draw_circle_filled(625, 325, 50, arcade.color.YELLOW)
-    arcade.draw_circle_filled(x, y, 5, arcade.color.BLUE)
-    if len(tegn_baner.spor) > SPORLAENGDE:
-        tegn_baner.spor.pop(0)
-    for punkt in tegn_baner.spor:
-        arcade.draw_circle_filled(*punkt, 2, arcade.csscolor.RED)
-    tegn_baner.tid += delta_tid
-    tegn_baner.spor.append((x, y))
+def ellipse_venus(tid, centrum, a, b, vinkelhastighed, fase=0):
+    x_c, y_c = centrum
+    x = x_c + a * math.cos(vinkelhastighed * tid + fase)
+    y = y_c + b * math.sin(vinkelhastighed * tid + fase)
+    return x, y
 
+def ellipse_earth(tid, centrum, a, b, vinkelhastighed, fase=0):
+    x_c, y_c = centrum
+    x = x_c + a * math.cos(vinkelhastighed * tid + fase)
+    y = y_c + b * math.sin(vinkelhastighed * tid + fase)
+    return x, y
+
+def ellipse_mars(tid, centrum, a, b, vinkelhastighed, fase=0):
+    x_c, y_c = centrum
+    x = x_c + a * math.cos(vinkelhastighed * tid + fase)
+    y = y_c + b * math.sin(vinkelhastighed * tid + fase)
+    return x, y
+
+
+
+def baner(delta_tid):
+    arcade.start_render()
+    x, y = ellipse_mercury(baner.tid, (BREDDE / 2, HOEJDE / 2), 70, 47, 2)
+    x_2, y_2 = ellipse_venus(baner.tid, (BREDDE / 2, HOEJDE / 2), 107, 108.9, 0.78)
+    x_3, y_3 = ellipse_earth(baner.tid, (BREDDE / 2, HOEJDE / 2), 147.570, 152.1, 0.48)
+    x_4, y_4 = ellipse_mercury(baner.tid, (BREDDE / 2, HOEJDE / 2), 206, 249, 0.24)
+    arcade.draw_circle_filled(625, 325, 26.927, arcade.color.YELLOW)
+    #Merkur
+    arcade.draw_circle_filled(x, y, 4.879, arcade.color.GRAY)
+    #Venus
+    arcade.draw_circle_filled(x_2, y_2, 12.104, arcade.color.ORANGE)
+    #Jorden
+    arcade.draw_circle_filled(x_3, y_3, 12.742, arcade.color.BLUE)
+    #Mars
+    arcade.draw_circle_filled(x_4, y_4, 6.787, arcade.color.RED)
+
+
+    if len(baner.spor) > SPORLAENGDE:
+        baner.spor.pop(0)
+
+    if len(baner.spor2) > SPORLAENGDE:
+        baner.spor2.pop(0)
+
+    if len(baner.spor3) > SPORLAENGDE:
+        baner.spor3.pop(0)
+
+    if len(baner.spor4) > SPORLAENGDE:
+        baner.spor4.pop(0)
+
+    for punkt in baner.spor:
+        arcade.draw_circle_filled(*punkt, 2, arcade.color.GRAY)
+    for punkt in baner.spor2:
+        arcade.draw_circle_filled(*punkt, 2, arcade.color.ORANGE)
+    for punkt in baner.spor3:
+        arcade.draw_circle_filled(*punkt, 2, arcade.color.BLUE)
+    for punkt in baner.spor4:
+        arcade.draw_circle_filled(*punkt, 2, arcade.color.RED)
+    baner.tid += delta_tid
+    baner.spor.append((x, y))
+    baner.spor2.append((x_2, y_2))
+    baner.spor3.append((x_3, y_3))
+    baner.spor4.append((x_4, y_4))
 def main():
-    arcade.open_window(BREDDE, HOEJDE, "her")
+    arcade.open_window(BREDDE, HOEJDE, "De indre planeter")
     arcade.set_background_color(arcade.csscolor.BLACK)
 
-    tegn_baner.tid = 0.0
-    tegn_baner.spor = list()
-    arcade.schedule(tegn_baner, 1 / 60)
+    baner.tid = 0.0
+    baner.spor = list()
+    baner.spor2 = list()
+    baner.spor3 = list()
+    baner.spor4 = list()
+    arcade.schedule(baner, 1 / 60)
 
     arcade.run()
 
